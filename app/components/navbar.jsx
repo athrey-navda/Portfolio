2a6469795da91c6bc63d4e81885dca6efff3bad9
athrey-navda <athu.navada@gmail.com>
2024-03-11 02:14:38 -0400

    update: close menuoption in mobile view

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -135,6 +135,7 @@ export default function Navbar() {
                       key={item.name}
                       href={`#${item.id}`}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-500"
+                      onClick={() => setMobileMenuOpen(false)}
                     >
                       {item.name}
                     </a>

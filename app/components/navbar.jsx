b898d6e10c62643238c0760df61728e22c4679ab
athrey-navda <athu.navada@gmail.com>
2024-03-11 20:38:18 -0400

    fix: Navbar theme handling

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar() {
   }
 
   return (
-    <div className="">
+    <div className="text-black dark:text-white bg-white dark:bg-black">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
           className="flex items-center justify-between p-6 lg:px-8"

daedc1f7ebea3a9215624a782de97168a0d615a6
athrey-navda <athu.navada@gmail.com>
2024-07-21 01:36:03 -0400

    add: leetcode link

--- app/components/heroSection.jsx
+++ app/components/heroSection.jsx
@@ -165,6 +165,28 @@ export default function HeroSection() {
 
                         <span className="sr-only">Instagram</span>
                       </Link>
+                      <Link
+                        href="https://leetcode.com/u/athrey-navda/"
+                        target="_blank"
+                        className="mx-3 hover:text-gray-900 dark:hover:text-sky-300"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+                          />
+                        </svg>
+
+                        <span className="sr-only">LeetCode</span>
+                      </Link>
                       <div className="mx-3">
                         <Link
                           href="/file/ByndoorAthreyNavda_Resume.pdf"

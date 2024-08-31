a72b96681ee90324fc24bd681711fa865bb99bd8
athrey-navda <athu.navada@gmail.com>
2024-08-31 00:47:32 -0400

    issue:game value cache

--- app/api/gameData/route.js
+++ app/api/gameData/route.js
@@ -1,10 +1,3 @@
-"use server";
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 export async function GET() {
   try {
     const { data: games, error } = await supabase
@@ -18,7 +11,14 @@ export async function GET() {
 
     return new Response(JSON.stringify(games), {
       status: 200,
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, proxy-revalidate",
+        Expires: "0",
+        Pragma: "no-cache",
+        "Surrogate-Control": "no-store",
+      },
     });
   } catch (error) {
     console.error("Error fetching game count data:", error.message);
@@ -26,7 +26,14 @@ export async function GET() {
       JSON.stringify({ message: "Error fetching game count data" }),
       {
         status: 500,
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+          "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
+          Expires: "0",
+          Pragma: "no-cache",
+          "Surrogate-Control": "no-store",
+        },
       }
     );
   }
--- app/api/updateGameData/route.js
+++ app/api/updateGameData/route.js
@@ -1,40 +1,52 @@
-export async function GET() {
+"use server";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function POST(req) {
   try {
-    const { data: games, error } = await supabase
+    const { gameName } = await req.json();
+
+    console.log(`Updating game data for ${gameName}`);
+
+    const { data: gameData, error: fetchError } = await supabase
       .from("game_counts")
-      .select("*");
-    if (error) {
-      throw new Error(error.message);
+      .select("count")
+      .eq("name", gameName)
+      .single();
+
+    if (fetchError || !gameData) {
+      console.log(`Game ${gameName} not found.`);
+      return new Response(JSON.stringify({ message: "Game not found" }), {
+        status: 404,
+      });
     }
 
-    console.log(games);
+    const todayDate = new Date().toISOString().split("T")[0];
+    const currentCount = gameData.count || {};
 
-    return new Response(JSON.stringify(games), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Cache-Control":
-          "no-store, no-cache, must-revalidate, proxy-revalidate",
-        Expires: "0",
-        Pragma: "no-cache",
-        "Surrogate-Control": "no-store",
-      },
-    });
+    currentCount[todayDate] = (currentCount[todayDate] || 0) + 1;
+
+    console.log("Updating with currentCount:", currentCount);
+
+    const { error: updateError } = await supabase
+      .from("game_counts")
+      .update({ count: currentCount })
+      .eq("name", gameName);
+
+    if (updateError) {
+      console.error("Update error:", updateError);
+      throw new Error(updateError.message);
+    }
+
+    console.log(`Game data updated successfully.`);
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (error) {
-    console.error("Error fetching game count data:", error.message);
-    return new Response(
-      JSON.stringify({ message: "Error fetching game count data" }),
-      {
-        status: 500,
-        headers: {
-          "Content-Type": "application/json",
-          "Cache-Control":
-            "no-store, no-cache, must-revalidate, proxy-revalidate",
-          Expires: "0",
-          Pragma: "no-cache",
-          "Surrogate-Control": "no-store",
-        },
-      }
-    );
+    console.error("Error updating game data:", error.message);
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+    });
   }
 }
